8eafabb663b26454bd9f115283dcc79569ea7c4d
Svetlana Gohl <sm181183@mail.ru>
2023-08-31 01:15:48 +0300

    Начальный коммит Исключения

---
+++ src/main/java/pro/sky/gohl/skyprogohlexceptions/SkyproGohlExceptionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.gohl.skyprogohlexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproGohlExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproGohlExceptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/gohl/skyprogohlexceptions/SkyproGohlExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.gohl.skyprogohlexceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproGohlExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
